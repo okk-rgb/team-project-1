84f2f8b45c366cf0bc9b83ad6de6f90a8b235b66
yotubern1 <albionyt79@gmail.com>
2026-01-15 20:36:14 +0500

    Support component styles and assets added

--- src/components/support/Support.jsx
+++ src/components/support/Support.jsx
@@ -1,12 +1,23 @@
-
-
+import "./Support.css";
+import food from "../assets/food.jpg"; 
 
 const Support = () => {
-  return(
-    <div className="container">
+  return (
+    <div className="support">
+      <div className="support-left">
+        <img src={food} alt="food" />
+      </div>
 
+      <div className="support-right">
+        <h2>
+         <span>good food</span>
+          <br />
+          and local business.
+        </h2>
+        <button>Order Now</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Support
+export default Support;
